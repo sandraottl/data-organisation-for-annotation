40775ddc7b5e1c9295da26a0eb2b76bd4f78c465
Sandra O <15089632+SandraO@users.noreply.github.com>
2017-11-15 14:30:14 +0100

    Rename folder

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
-setup(name='de_enigma_scripts',
+setup(name='scripts',
       version='0.1.1',
       packages=find_packages(),
       install_requires=['soundfile'], 
       entry_points={
           'console_scripts': [
-              'remove_overlaps = de_enigma_scripts.remove_overlaps:main',
-              'insert_annotations = de_enigma_scripts.insert_annotations:main',
-              'organise_timestamps = de_enigma_scripts.organise_timestamps:main',
-              'split_videos = de_enigma_scripts.split_videos:main',
-              'fleiss_kappa = de_enigma_scripts.fleiss_kappa:main'
+              'remove_overlaps = scripts.remove_overlaps:main',
+              'insert_annotations = scripts.insert_annotations:main',
+              'organise_timestamps = scripts.organise_timestamps:main',
+              'split_videos = scripts.split_videos:main',
+              'fleiss_kappa = scripts.fleiss_kappa:main'
           ]
       },
 
